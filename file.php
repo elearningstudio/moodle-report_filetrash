0f9e96e813e42b2ffce13c63039d73d68f5812fd
Barry Oosthuizen <barry@elearningstudio.co.uk>
2013-08-16 20:05:03 +0100

    Use readfile_chunked function instead of readfile

--- file.php
+++ file.php
@@ -46,4 +46,28 @@ header('Content-Length: ' . filesize($path));
 header("Cache-Control: private");
 
 header("Content-Disposition: attachment; filename=" . $filename);
-readfile($path);
+readfile_chunked($path);
+
+function readfile_chunked($filename, $retbytes = true) {
+    $chunksize = 1 * (1024 * 1024);
+    $buffer = '';
+    $cnt = 0;
+    $handle = fopen($filename, 'rb');
+    if ($handle === false) {
+        return false;
+    }
+    while (!feof($handle)) {
+        $buffer = fread($handle, $chunksize);
+        echo $buffer;
+        ob_flush();
+        flush();
+        if ($retbytes) {
+            $cnt += strlen($buffer);
+        }
+    }
+    $status = fclose($handle);
+    if ($retbytes && $status) {
+        return $cnt;
+    }
+    return $status;
+}
