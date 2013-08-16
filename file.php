af9862ee627825bad09839519148238de9d4d78f
Barry Oosthuizen <barry@elearningstudio.co.uk>
2013-08-16 21:18:21 +0100

    require_once

--- file.php
+++ file.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require('../../config.php');
+require_once('../../config.php');
 
 require_once($CFG->dirroot . '/report/filetrash/lib.php');
 
