b7b83aa8d6250506c9297c6e82663d4de8b4805b
Barry Oosthuizen <barry@elearningstudio.co.uk>
2013-08-18 18:49:14 +0100

    Initial commit

---
+++ settings.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of the File Trash report by Barry Oosthuizen - http://elearningstudio.co.uk
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Links and settings
+ *
+ * This file contains links and settings used by report_filetrash
+ *
+ * @package    report_filetrash
+ * @copyright  2013 Barry Oosthuizen
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$ADMIN->add('reports', new admin_externalpage('reportfiletrash',
+        get_string('pluginname', 'report_filetrash'),
+        "$CFG->wwwroot/report/filetrash/index.php", 'report/filetrash:view'));
+
+$settings = null;
